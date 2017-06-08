c1fd08d69f3b448adbd3ff8e7f839588e446dc76
kirsten knapp and kirstin hartman <kirsten+kirstin@>
2017-06-07 18:54:50 -0700

    Add new working jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   $("button#btn-monkey").click(function() {
-    $("#monkey-id").toggle();
+    $("#monkey-id").show();
+    $("#img-monkey").click(function() {
+    $("#monkey-id").hide();
+    });
+  });
+  $("button#btn-mouse").click(function() {
+    $("#mouse-id").show();
+    $("#img-mouse").click(function() {
+    $("#mouse-id").hide();
+  });
+});
+$("button#btn-kitty").click(function() {
+  $("#kitty-id").show();
+  $("#img-kitty").click(function() {
+  $("#kitty-id").hide();
+    });
   });
 });
